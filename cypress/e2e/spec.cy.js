da52b89c867ade98a1b658fcc27be1476530fc48
Dell <kadi.marss@voco.ee>
2022-11-11 16:18:16 +0200

    github actions tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,9 +3,9 @@ describe('Testing api', () => {
         cy.visit('http://127.0.0.1:5173/users');
         cy.get('main > div > form > fieldset.form-group.form > input').click();
         cy.wait(1000);
-        cy.get('main > div > form > fieldset.form-group.form > input').type('kapsas@mail.com');
+        cy.get('main > div > form > fieldset.form-group.form > input').type('roosa@mail.com');
         cy.wait(1000);
-        cy.get('main > div > form > fieldset:nth-child(2) > input').type('kapsas');
+        cy.get('main > div > form > fieldset:nth-child(2) > input').type('roosa');
         cy.wait(1000);
         cy.get('.form-group:nth-child(3) > .form-control').click();
         cy.get('.form-group:nth-child(3) > .form-control').type('Kaskkas7311!!');
@@ -21,13 +21,13 @@ describe('Testing api', () => {
         cy.get('form:nth-child(1) > .form-group:nth-child(1) > .form-control').click();
         cy.get('form:nth-child(1) > .form-group:nth-child(1) > .form-control').clear();
         cy.wait(1000);
-        cy.get('form:nth-child(1) > .form-group:nth-child(1) > .form-control').type('peet@mail.ee');
+        cy.get('form:nth-child(1) > .form-group:nth-child(1) > .form-control').type('must@mail.ee');
         cy.wait(1000);
         cy.get('div.modal > div > div > form > fieldset:nth-child(2) > input').click();
         cy.wait(1000);
         cy.get('div.modal > div > div > form > fieldset:nth-child(2) > input').clear();
         cy.wait(1000);
-        cy.get('div.modal > div > div > form > fieldset:nth-child(2) > input').type('peet');
+        cy.get('div.modal > div > div > form > fieldset:nth-child(2) > input').type('must');
         cy.get('div.modal input.password').click();
         cy.wait(1000);
         cy.get('div.modal input.password').clear();
